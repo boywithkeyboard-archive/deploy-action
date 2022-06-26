72f54f8104ceb9b46ae412b701d78af6c746a2d2
Azury <azury.wtf@gmail.com>
2022-06-26 23:11:22 +0200

    added main file

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { NodeSSH } from 'node-ssh'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { getInput, setFailed } from '@actions/core'
+import { isIPv4 } from 'node:net'
+
+try {
+  // validate inputs
+  const host = getInput('host', { required: true })
+  const port = getInput('port')
+  const username = getInput('username', { required: true })
+  const privateKey = getInput('privateKey', { required: true })
+  const pm2 = getInput('pm2')
+  const dependencies = getInput('dependencies') === 'true' ? true : false
+  const destination = getInput('destination', { required: true })
+
+  if (!isIPv4(host))
+    throw new Error('Invalid Hostname')
+
+  // deploy app
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  , ssh = new NodeSSH()
+  
+  await ssh.connect({
+    host,
+    port: parseInt(port),
+    username,
+    privateKey
+  })
+  
+  await ssh.putFile(join(__dirname, './dist/index.js'), `${destination}/dist/index.js`)
+  
+  if (dependencies) {
+    await ssh.putFile(join(__dirname, './package.json'), `${destination}/package.json`)
+    await ssh.putFile(join(__dirname, './package-lock.json'), `${destination}/package-lock.json`)
+
+    await ssh.execCommand('npm i', { cwd: destination })
+  }
+  
+  if (pm2)
+    await ssh.execCommand(`pm2 restart ${pm2} --cron-restart="0 0 * * *" --exp-backoff-restart-delay=100`)
+  
+  if (ssh.connection)
+    ssh.connection.end()
+} catch (err) {
+  if (err instanceof Error)
+    setFailed(err.message)
+  else
+    setFailed('Unknown Error')
+} finally {
+  process.exit()
+}
